845b42fb058ee66951dadc61dc9f46e5f9b5a3f8
Matthew Kitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-30 18:32:27 -0400

    chore: Responsive Layout Component (#12)
    * chore: Responsive Layout Component
    
    * Update src/index.css
    
    Co-authored-by: Will Johnston <wijohnst@gmail.com>
    
    * Update src/Layout.js
    
    Co-authored-by: Will Johnston <wijohnst@gmail.com>
    
    ---------
    
    Co-authored-by: Will Johnston <wijohnst@gmail.com>

---
+++ src/Layout.js
@@ -0,0 +1,8 @@
+const Layout = ({ children }) => {
+  return (
+    <div className="bg-white max-w-screen-xl mx-auto h-screen">
+      <span>JavaScripture</span>
+      {props.children}
+    </div>
+  );
+}

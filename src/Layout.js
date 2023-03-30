11603ae09c8a5f206cbced974f69c705e214fb22
Matthew Kitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-30 18:26:29 -0400

    Update src/Layout.js
    Co-authored-by: Will Johnston <wijohnst@gmail.com>

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,4 @@
-export default function Layout(props) {
+const Layout = ({ children }) => {
   return (
     <div className="bg-white max-w-screen-xl mx-auto h-screen">
       <span>JavaScripture</span>
